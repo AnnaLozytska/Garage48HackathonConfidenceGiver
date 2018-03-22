07122663de66f2dcfc15617ee5c2d33190585add
Anna Lozytska <anna.lozytska@lookout.com>
2018-03-22 18:49:36 +0200

    Added Splash screen

---
+++ app/src/main/java/com/example/annalozytska/confidencegiver/onboarding/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.annalozytska.confidencegiver.onboarding;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.annalozytska.confidencegiver.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
